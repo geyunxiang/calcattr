46a068c6f6c3aefb7abf50489b0182de3fade986
geyunxiang <geyunxiang_95@sina.com>
2017-10-18 17:02:35 +0800

    Update: code format

--- xzfn_write_simple_csv.m
+++ xzfn_write_simple_csv.m
@@ -1,10 +1,10 @@
-function xzfn_write_simple_csv(outfile, header, thedata)
-    cntrow = size(thedata,1);
+function xzfn_write_simple_csv(outfile, header, data)
+    cntrow = size(data, 1);
     
-    foutcsv = fopen(outfile,'w');
-    fprintf(foutcsv, [header,'\n']);
-    for i=1:cntrow
-        fprintf(foutcsv,[num2str(thedata(i)),'\n']);
+    foutcsv = fopen(outfile, 'w');
+    fprintf(foutcsv, [header, '\n']);
+    for i = 1:cntrow
+        fprintf(foutcsv, [num2str(data(i)), '\n']);
     end
     fclose(foutcsv);
 
